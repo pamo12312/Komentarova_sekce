75e6732ddf522e81605795e9379567c2dbc5ddc3
Patrik <patri.moravec@email.cz>
2023-12-16 16:42:54 +0100

    přidání smazání kometáře

--- js.js
+++ js.js
@@ -1,50 +1,55 @@
 const comments = [
     {
-        name: 'Denis',
+        name: 'Denisa',
         date: '2023-12-12T08:30:00Z',
         text: 'Skvělý článek!',
         image: 'aioni.jpg'
     },
     {
-        name: 'Evžen',
-        date: '2023-12-12 10:45:00',
+        name: 'Aneta',
+        date: '2023-12-12T10:45:00Z',
         text: 'Díky za sdílení.',
         image: 'michael.jpg'
     },
     {
-        name: ' Tomáš',
-        date: '2023-12-12 12:15',
+        name: 'Denis',
+        date: '2023-12-12T12:15:00Z',
         text: 'Moc pěkné.',
         image: 'jan.jpg'
     }
 ];
 
+const container = document.getElementById('comments-container');
 
-function addComment(comment) {
-    const container = document.getElementById('comments-container');
-
+const addComment = (comment, index) => {
     const commentElement = document.createElement('div');
-    const commentId = Date.now();
-    commentElement.id = `comment-${commentId}`;
+    commentElement.id = `comment-${index}`;
     commentElement.innerHTML = `
-        <img src="${comment.image}" alt="${comment.name}" style="width: 50px; height: 50px; border-radius: 50%;object-fit: cover">
-        <b>${comment.name}</b> (${new Date(comment.date).toLocaleDateString()})
-        <p>${comment.text}</p>
-       
-        <hr> `;
+    <img src="${comment.image}" alt="${comment.name}" style="width: 50px; height: 50px; border-radius: 50%; object-fit: cover">
+    <b>${comment.name}</b> (${new Date(comment.date).toLocaleDateString()})
+    <p>${comment.text}</p>
+    <button class="delete-button">Smazat</button>
+    <hr> `;
 
     container.appendChild(commentElement);
-}
+};
 
+const deleteComment = (index) => {
+    comments.splice(index, 1);
+    displayComments();
+};
 
-function displayComments() {
-    comments.forEach(comment => {
-        addComment(comment);
+const displayComments = () => {
+    container.innerHTML = '';
+    comments.forEach((comment, index) => {
+        addComment(comment, index);
     });
-}
 
 
+    const deleteButtons = document.querySelectorAll('.delete-button');
+    deleteButtons.forEach((button, index) => {
+        button.addEventListener('click', () => deleteComment(index));
+    });
+};
 
-
-
-displayComments();
+document.addEventListener('DOMContentLoaded', displayComments);
